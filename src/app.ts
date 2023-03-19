de5520fee291110fc0dfcf4c95e0c8751620a852
EstherABA <120097060+EstherABA@users.noreply.github.com>
2023-03-19 16:11:15 +0200

    env gitIgnore

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv'
 
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
